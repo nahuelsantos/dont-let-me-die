555610abcfe218124b7ea7754ab07d5523aa47d1
Nahuel <nahuelsantos@gmail.com>
2022-08-19 19:21:04 +0200

    Updated file location path.

--- src/utils.js
+++ src/utils.js
@@ -81,7 +81,7 @@ export const sleep = (ms = 500) =>
   new Promise((resolve, _reject) => setTimeout(resolve, ms));
 
 export const loadQuestions = async (difficulty = "easy") => {
-  return JSON.parse(await readFile("src/data/questions.json", "utf8")).results.filter(q => q.difficulty === difficulty);
+  return JSON.parse(await readFile("./src/data/questions.json", "utf8")).results.filter(q => q.difficulty === difficulty);
 }
 
 export const shuffle = (array) => {

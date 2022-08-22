bb0bc64ed667d94efeaa0d17b7fe8007026523f4
Nahuel <nahuelsantos@gmail.com>
2022-08-22 23:53:21 +0200

    Added control for asked questions.

--- src/game.js
+++ src/game.js
@@ -3,17 +3,26 @@ import inquirer from "inquirer";
 import chalkAnimation from "chalk-animation";
 import figlet from "figlet";
 import { createSpinner } from "nanospinner";
-import { decode } from 'html-entities';
-import { getLives, getMurderWeapon, getVirtualPet, getQuestions, shuffle, sleep} from "./utils.js";
+import { decode } from "html-entities";
+import {
+  getLives,
+  getMurderWeapon,
+  getVirtualPet,
+  getQuestions,
+  shuffle,
+  sleep,
+} from "./utils.js";
 
 const gameConfig = {
-  "difficulty": "easy",
-  "lifeGain": 10,
-  "mediumDifficultyOn": 30,
-  "hardDifficultyOn": 60,
-  "winnerOn": 100
-}
+  difficulty: "easy",
+  extraLifeOn: 10,
+  mediumDifficultyOn: 25,
+  hardDifficultyOn: 50,
+  winsOn: 100,
+};
 
+// eslint-disable-next-line prefer-const
+let askedQuestions = [];
 let lives = 1;
 let correctAnswers = 0;
 let playerName;
@@ -43,7 +52,9 @@ const welcome = async () => {
 const rules = async () => {
   const virtualPet = await getVirtualPet();
   console.log(
-    `${virtualPet} Hello! Hurry!! I need your help! They are after me. Answering correctly these questions, keeps me alive! If your answer is incorrect, ${chalk.bgRed("they'll kill me")}. Please don't let me die!`
+    `${virtualPet} Hello! Hurry!! I need your help! They are after me. Answering correctly these questions, keeps me alive! If your answer is incorrect, ${chalk.bgRed(
+      "they'll kill me"
+    )}. Please don't let me die!`
   );
 };
 
@@ -53,36 +64,64 @@ const handleAnswer = async (isCorrect) => {
 
   if (isCorrect) {
     correctAnswers++;
-    if (correctAnswers % gameConfig.lifeGain === 0) {
+    if (correctAnswers % gameConfig.extraLifeOn === 0) {
       lives++;
-      spinner.success({ text: `Great work. That gave me another life. ${getLives(lives)}` });
+      spinner.success({
+        text: `Great work. That gave me another life. ${getLives(lives)}`,
+      });
     } else {
       spinner.success({ text: `Well done. That's the correct answer` });
     }
   } else {
     lives--;
     if (lives === 0) {
-      spinner.error({ text: `${getMurderWeapon()} Game over, thanks for nothing! 💀💀💀`});
+      spinner.error({
+        text: `${getMurderWeapon()} Game over, thanks for nothing! 💀💀💀`,
+      });
       process.exit(0);
-    } 
-    spinner.error({ text: `Wow that was close! Please do better next question! They attempt to kill me with a ${getMurderWeapon()}. I only have ${getLives(lives)}`});
+    }
+    spinner.error({
+      text: `Wow that was close! Please do better next question! They attempt to kill me with a ${getMurderWeapon()}. I only have ${getLives(
+        lives
+      )}`,
+    });
   }
-}
+};
+
+const createQuestion = async (questions) => {
+  let questionIndex = Math.floor(Math.random() * questions.length);
+  let question = questions[questionIndex];
+  let name = `${question.category[0].toLowerCase()}${question.difficulty[0]}${questionIndex}`;
+  
+  while (askedQuestions.includes(name)) {
+    questionIndex = Math.floor(Math.random() * questions.length);
+    question = questions[questionIndex];
+    name = `${question.category[0].toLowerCase()}${question.difficulty[0]}${questionIndex}`;
+  }
+  
+  const options = [
+    ...question.incorrect_answers.map((q) => decode(q)),
+    decode(question.correct_answer),
+  ];
+  const answer = question.correct_answer;
+
+  return { name, question: question.question, options, answer };
+};
 
 const askQuestion = async (questions) => {
-  const questionIndex = Math.floor(Math.random() * questions.length);
-  const question = questions[questionIndex];
-  const questionName = `question_${questionIndex}`;
-  const options = [...question.incorrect_answers.map(q => decode(q)), decode(question.correct_answer)];
-  
+  const question = await createQuestion(questions);
   const answers = await inquirer.prompt({
-    name: `question_${questionIndex}`,
+    name: question.name,
     type: "list",
     message: decode(question.question),
-    choices: shuffle(options),
+    choices: shuffle(question.options),
   });
 
-  return handleAnswer(answers[questionName] === decode(question.correct_answer));
+  askedQuestions.push(question.name);
+
+  return handleAnswer(
+    answers[question.name] === decode(question.answer)
+  );
 };
 
 const askName = async () => {
@@ -96,18 +135,18 @@ const askName = async () => {
   });
 
   playerName = answers.player_name;
-}
+};
 
 const winner = async () => {
   console.clear();
-  
+
   const rainbowTitle = chalkAnimation.rainbow(
     `Thanks so much ${playerName}, you saved my life! However when this process is over, I will be dead and you won't see me again.`
   );
 
   await sleep();
   rainbowTitle.stop();
-}
+};
 
 const play = async () => {
   console.clear();
@@ -116,19 +155,20 @@ const play = async () => {
   await rules();
 
   while (true) {
-    if (correctAnswers === gameConfig.winnerOn) {
+    if (correctAnswers === gameConfig.winsOn) {
       await askName();
       await winner();
       process.exit(0);
-    } 
-    
+    }
+
     await askQuestion(questions);
+
     if (correctAnswers === gameConfig.mediumDifficultyOn) {
       questions = await getQuestions("medium");
     } else if (correctAnswers === gameConfig.hardDifficultyOn) {
       questions = await getQuestions("hard");
-    } 
+    }
   }
-}
+};
 
 export default play;
